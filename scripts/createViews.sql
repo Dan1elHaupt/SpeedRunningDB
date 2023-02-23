79d2dfc38d1d1105b31118a9cbd21eedeb7441e4
Daniel Shuttleworth <daniels@bbd.co.za>
2023-02-23 17:24:17 +0200

    Updated view script, credit to Dané

--- scripts/createViews.sql
+++ scripts/createViews.sql
@@ -1,18 +1,20 @@
 USE SpeedRunnerDB
 GO
 
-CREATE OR ALTER VIEW Top_Runner
-AS
-SELECT DISTINCT game_name = FIRST_VALUE(game_name) OVER (PARTITION BY dbo.Runs.category_id ORDER BY run_time ASC),
-runner_name,
-category_name,
-run_time,
-video_link,
-case when validity=1 then 'true' 
-           else 'false' 
-           end as validity
-FROM dbo.Runs INNER JOIN dbo.Games ON Runs.game_id = Games.game_id INNER JOIN dbo.Categories ON Runs.category_id = Categories.category_id
-	INNER JOIN dbo.Runners on Runs.runner_id = Runners.runner_id
-WHERE Runs.deleted = 0
+CREATE OR ALTER VIEW Top_Runner AS
+
+WITH CTE AS(
+SELECT runner_id,region_id,category_id,game_id,run_time, ROW_NUMBER() OVER (PARTITION BY category_id,game_id ORDER BY run_time ASC) AS rn
+FROM dbo.Runs WHERE validity = 1 group by category_id,game_id ,run_time,runner_id,region_id),
+
+CTE2 as(
+SELECT r.runner_name,n.region_name,t.category_name,g.game_name,(run_time/360) as run_time_hours , run_time as run_time_seconds ,rn FROM CTE
+inner join Categories as t on t.category_id = CTE.category_id
+inner join Games as g on g.game_id = CTE.game_id
+inner join Runners as r on r.runner_id = CTE.runner_id
+inner join Regions as n on n.region_id = CTE.region_id
+)
+
+SELECT runner_name,region_name,category_name,game_name,run_time_hours,run_time_seconds FROM CTE2 WHERE rn = 1
 
 GO

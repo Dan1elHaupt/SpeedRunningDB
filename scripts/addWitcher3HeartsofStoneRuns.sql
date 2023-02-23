ad71b7b819c83013f8b18f0990c414bebd8a4df0
Daniel Shuttleworth <daniels@bbd.co.za>
2023-02-23 16:54:35 +0200

    More improvements to scraping script and run adding scripts

---
+++ scripts/addWitcher3HeartsofStoneRuns.sql
@@ -0,0 +1,69 @@
+INSERT INTO dbo.Runs
+        (runner_id
+        ,game_id
+        ,category_id
+        ,platform_id
+        ,region_id
+        ,run_time
+        ,in_game_time
+        ,game_version
+        ,date_posted
+        ,video_link
+        ,validity
+        ,verified_by
+        )
+VALUES	(3, 2, 3, 4, 1, 2544, 0, 4.01, '2017-07-06T05:38:08Z', 'https://www.youtube.com/watch?v=IXLf7IA5aNs', 1, 2),
+        (2, 2, 3, 4, 1, 3283, 0, 4.01, '2017-08-10T05:23:10Z', 'https://www.twitch.tv/videos/165826864', 1, 2),
+        (2, 2, 3, 4, 1, 2982, 0, 4.01, '2018-03-21T21:18:56Z', 'https://www.twitch.tv/videos/241314389', 1, 2),
+        (3, 2, 3, 4, 1, 2585, 0, 4.01, '2018-04-20T18:15:13Z', 'https://www.twitch.tv/videos/252522490', 1, 1),
+        (3, 2, 3, 4, 1, 2199, 0, 4.01, '2018-04-21T08:37:01Z', 'https://www.twitch.tv/videos/252779171', 1, 1),
+        (4, 2, 3, 4, 1, 2164, 0, 4.01, '2018-05-11T16:24:01Z', 'https://www.twitch.tv/videos/260195755', 1, 2),
+        (3, 2, 3, 4, 1, 2455, 0, 4.01, '2019-02-03T16:18:04Z', 'https://www.twitch.tv/videos/374612331', 1, 2),
+        (3, 2, 3, 4, 1, 2195, 0, 4.01, '2019-02-18T20:47:33Z', 'https://www.twitch.tv/videos/382904087', 1, 1),
+        (1, 2, 3, 4, 1, 2099, 0, 4.01, '2019-03-23T01:50:57Z', 'https://www.youtube.com/watch?v=Pd26BR7fxBs', 1, 2),
+        (2, 2, 3, 4, 1, 2831, 0, 4.01, '2019-03-23T07:15:40Z', 'https://www.twitch.tv/videos/399802298', 1, 2),
+        (1, 2, 3, 4, 1, 2575, 0, 4.01, '2019-03-25T08:15:15Z', 'https://www.twitch.tv/videos/400908032', 1, 1),
+        (2, 2, 3, 4, 1, 2069, 0, 4.01, '2019-03-25T15:49:57Z', 'https://www.youtube.com/watch?v=wmXziT91YcA', 1, 2),
+        (1, 2, 3, 4, 1, 2019, 0, 4.01, '2019-03-29T23:46:40Z', 'https://www.youtube.com/watch?v=M2-D7THBt48', 1, 2),
+        (4, 2, 3, 4, 1, 1933, 0, 4.01, '2019-04-06T11:49:32Z', 'https://www.youtube.com/watch?v=l54A6g_WGJw', 1, 1),
+        (2, 2, 3, 4, 1, 2049, 0, 4.01, '2020-04-01T05:28:04Z', 'https://youtu.be/PSyjasMJqBw', 1, 1),
+        (2, 2, 3, 4, 1, 1977, 0, 4.01, '2020-04-02T05:31:22Z', 'https://youtu.be/1By0lFns9dc', 1, 2),
+        (4, 2, 3, 4, 1, 2101, 0, 4.01, '2020-04-10T00:30:51Z', 'https://youtu.be/dvAjQ3nwxqA', 1, 2),
+        (4, 2, 3, 4, 1, 2071, 0, 4.01, '2020-04-15T01:59:21Z', 'https://youtu.be/KSFBPDTZ2gI', 1, 1),
+        (3, 2, 3, 4, 1, 2026, 0, 4.01, '2020-04-17T15:03:11Z', 'https://youtu.be/MxtFh2-xp2I', 1, 1),
+        (3, 2, 3, 4, 1, 3047, 0, 4.01, '2020-04-20T21:18:40Z', 'https://www.youtube.com/watch?v=492BEwlh4E4', 1, 1),
+        (3, 2, 3, 4, 1, 2750, 0, 4.01, '2020-04-21T18:13:47Z', 'https://www.youtube.com/watch?v=ba7cKuTnano', 1, 1),
+        (2, 2, 3, 4, 1, 1899, 0, 4.01, '2020-04-21T22:24:57Z', 'https://youtu.be/PJXhQ8vW0hQ', 1, 1),
+        (2, 2, 3, 4, 1, 2559, 0, 4.01, '2020-04-22T12:40:01Z', 'https://youtu.be/jFcBGUdB7Pw', 1, 2),
+        (2, 2, 3, 4, 1, 2468, 0, 4.01, '2020-04-22T16:39:47Z', 'https://www.youtube.com/watch?v=0j8ZRLV351I', 1, 2),
+        (3, 2, 3, 4, 1, 2327, 0, 4.01, '2020-04-22T20:23:27Z', 'https://www.youtube.com/watch?v=OrMImReqRJk', 1, 2),
+        (3, 2, 3, 4, 1, 2053, 0, 4.01, '2020-04-23T11:34:40Z', 'https://www.twitch.tv/videos/600147299', 1, 1),
+        (3, 2, 3, 4, 1, 2032, 0, 4.01, '2020-04-28T13:28:11Z', 'https://www.youtube.com/watch?v=ldCfHmDoDx0', 1, 1),
+        (2, 2, 3, 4, 1, 1824, 0, 4.01, '2020-04-29T22:42:27Z', 'https://www.youtube.com/watch?v=wWdc4GHHRBc', 1, 2),
+        (1, 2, 3, 4, 1, 1921, 0, 4.01, '2020-05-18T15:20:58Z', 'https://www.youtube.com/watch?v=50M-8V-ZGnE', 1, 1),
+        (3, 2, 3, 6, 1, 4678, 0, 4.01, '2020-05-26T13:39:33Z', 'https://www.youtube.com/watch?v=jv9RYh9YfTQ', 1, 2),
+        (3, 2, 3, 4, 1, 2336, 0, 4.01, '2020-05-29T01:39:41Z', 'https://youtu.be/vlNAeiiADbU', 1, 2),
+        (3, 2, 3, 4, 1, 2037, 0, 4.01, '2020-06-04T03:42:37Z', 'https://youtu.be/6u7XgLsesLE', 1, 2),
+        (1, 2, 3, 4, 1, 2212, 0, 4.01, '2020-09-05T19:46:41Z', 'https://youtu.be/yUB82wpe76k', 1, 2),
+        (2, 2, 3, 4, 1, 2713, 0, 4.01, '2020-10-23T19:23:25Z', 'https://www.twitch.tv/videos/779514471', 1, 1),
+        (3, 2, 3, 4, 1, 2527, 0, 4.01, '2020-11-07T07:10:26Z', 'https://www.youtube.com/watch?v=qaaJixshAfU', 1, 1),
+        (2, 2, 3, 4, 1, 2008, 0, 4.01, '2020-11-29T14:08:47Z', 'https://www.youtube.com/watch?v=08Wtmo_TQAo', 1, 2),
+        (2, 2, 3, 4, 1, 1816, 0, 4.01, '2020-12-10T18:12:57Z', 'https://youtu.be/JKYq69_cWMw', 1, 2),
+        (3, 2, 3, 4, 1, 1766, 1766, 4.01, '2020-12-17T04:11:38Z', 'https://youtu.be/vb0algVQCng', 1, 2),
+        (1, 2, 3, 4, 1, 1768, 0, 4.01, '2021-01-16T15:32:06Z', 'https://www.youtube.com/watch?v=2CAyuZyDyVg', 1, 1),
+        (4, 2, 3, 4, 1, 1904, 0, 4.01, '2021-03-30T17:43:27Z', 'https://youtu.be/5ulMkLNInqA', 1, 1),
+        (3, 2, 3, 4, 1, 1810, 0, 4.01, '2021-05-11T17:22:49Z', 'https://youtu.be/jmtgs26UYRI', 1, 1),
+        (4, 2, 3, 4, 1, 1734, 0, 4.01, '2021-05-15T02:05:42Z', 'https://youtu.be/KiixneS2MTE', 1, 1),
+        (1, 2, 3, 4, 1, 1716, 0, 4.01, '2021-05-17T15:40:14Z', 'https://www.youtube.com/watch?v=dLtNAZISKmk', 1, 2),
+        (1, 2, 3, 4, 1, 1951, 0, 4.01, '2021-08-19T20:27:48Z', 'https://youtu.be/HT9H5MoPt40', 1, 2),
+        (1, 2, 3, 4, 1, 5772, 0, 4.01, '2021-08-25T09:24:13Z', 'https://youtu.be/MCIIsE-W8pY', 1, 2),
+        (2, 2, 3, 4, 1, 3943, 0, 4.01, '2021-10-25T15:55:17Z', 'https://www.youtube.com/watch?v=ElcFUnsf3n0', 1, 1),
+        (1, 2, 3, 4, 1, 2226, 0, 4.01, '2021-11-22T20:28:50Z', 'https://www.youtube.com/watch?v=kA7IWYjyfwY', 1, 1),
+        (4, 2, 3, 4, 1, 2585, 0, 4.01, '2021-11-24T20:26:45Z', 'https://www.youtube.com/watch?v=QN9qXSCGQCc&t=1127s', 1, 2),
+        (2, 2, 3, 4, 1, 1777, 0, 4.01, '2021-12-06T23:15:34Z', 'https://www.twitch.tv/videos/1226207235', 1, 2),
+        (2, 2, 3, 4, 1, 3497, 0, 4.01, '2022-03-27T17:52:29Z', 'https://youtu.be/XtnIBM1IzwQ', 1, 1),
+        (3, 2, 3, 4, 1, 2998, 0, 4.01, '2022-07-07T13:07:18Z', 'https://www.youtube.com/watch?v=5rzuiJRZnwI', 1, 2),
+        (3, 2, 3, 4, 1, 1706, 0, 4.01, '2022-08-03T21:27:45Z', 'https://youtu.be/AYCUCbPEXZg', 1, 1),
+        (4, 2, 3, 4, 1, 2830, 0, 4.01, '2022-08-22T14:52:14Z', 'https://www.youtube.com/watch?v=03EirUFtuNE', 1, 2),
+        (2, 2, 3, 4, 1, 2490, 0, 4.01, '2022-09-03T16:41:14Z', 'https://www.youtube.com/watch?v=CrzmgjwBI0M', 1, 2)
+GO
--- scripts/speedrunScraper.js
+++ scripts/speedrunScraper.js
@@ -10,38 +10,101 @@ platforms = {
   "Nintendo Switch": 9,
 };
 
-// const game = "om1mx362"; //GTA V
-const game = "k6qre01g"; //The Witcher 3
+runStatuses = {
+  new: 0,
+  verified: 1,
+  rejected: 2,
+};
 
-// const category = "wkpn1jdr"; //Any% GTA V
-const category = "jdr7j9n2"; //Any% Witcher 3
+// GTA V:          1
+// The Witcher 3:  2
+// ############
+const game = 2;
+// ############
+let gameID;
+let gameVersion;
+switch (game) {
+  case 1:
+    gameID = "om1mx362";
+    gameVersion = 1.64;
+    break;
+  case 2:
+    gameID = "k6qre01g";
+    gameVersion = 4.01;
+    break;
+}
+
+// Any%:            1
+// Segments:        2
+// Hearts of Stone: 3
+// ################
+const category = 3;
+// ################
+let categoryID;
+let secondLast;
+switch (category) {
+  case 1:
+    if (game == 1) {
+      categoryID = "wkpn1jdr";
+      secondLast = 600;
+    } else {
+      categoryID = "jdr7j9n2";
+      secondLast = 0;
+    }
+    break;
+  case 2:
+    if (game == 1) {
+      categoryID = "7kjvmgk3";
+      secondLast = 3400;
+    } else {
+      throw "This game does not have this category";
+    }
+    break;
+  case 3:
+    if (game == 2) {
+      categoryID = "w201nqvk";
+      secondLast = 0;
+    } else {
+      throw "This game does not have this category";
+    }
+}
 
 async function getResults() {
-  const response = await fetch(
-    `https://www.speedrun.com/api/v1/runs?max=200&offset=0&game=${game}&category=${category}`
-  );
-  const data = await response.json();
-  for (let i = 0; i < 200; i++) {
-    if (data.data[i].system !== undefined) {
-      let platform = await fetch(
-        `https://www.speedrun.com/api/v1/platforms/${data.data[i].system.platform}`
-      );
-      platform = await platform.json();
-      if (
-        data.data[i].submitted !== null &&
-        platforms[platform.data.name] !== undefined &&
-        data.data[i].videos.links !== undefined
-      ) {
-        // console.log(data.data[i]);
-        console.log(
-          `(${Math.round(Math.random() * 3 + 1)}, 2, 1, ${
-            platforms[platform.data.name]
-          }, 1, ${data.data[i].times.primary_t}, ${
-            data.data[i].times.ingame_t
-          }, 4.01, '${data.data[i].submitted}', '${
-            data.data[i].videos.links[0].uri
-          }', ${data.data[i].status.status == "verified" ? 1 : 0}),`
+  for (let off = 0; off <= 200; off += 200) {
+    const response = await fetch(
+      `https://www.speedrun.com/api/v1/runs?max=200&offset=${
+        secondLast + off
+      }&game=${gameID}&category=${categoryID}`
+    );
+    const data = await response.json();
+    for (let i = 0; i < 200; i++) {
+      if (data.data[i].system !== undefined) {
+        let platform = await fetch(
+          `https://www.speedrun.com/api/v1/platforms/${data.data[i].system.platform}`
         );
+        platform = await platform.json();
+        if (
+          data.data[i].submitted !== null &&
+          platforms[platform.data.name] !== undefined &&
+          data.data[i].videos.links !== undefined
+        ) {
+          let verifiedBy;
+          if (data.data[i].status.status == "new") {
+            verifiedBy = null;
+          } else {
+            verifiedBy = Math.round(Math.random() + 1);
+          }
+          // console.log(data.data[i].status.status);
+          console.log(
+            `(${Math.round(Math.random() * 3 + 1)}, ${game}, ${category}, ${
+              platforms[platform.data.name]
+            }, 1, ${data.data[i].times.primary_t}, ${
+              data.data[i].times.ingame_t
+            }, ${gameVersion}, '${data.data[i].submitted}', '${
+              data.data[i].videos.links[0].uri
+            }', ${runStatuses[data.data[i].status.status]}, ${verifiedBy}),`
+          );
+        }
       }
     }
   }

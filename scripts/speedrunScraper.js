bad37ca29551f94263f7d2d270a54f0562fc9f9a
Daniel Shuttleworth <daniels@bbd.co.za>
2023-02-23 11:51:57 +0200

    Improved API script and created scripts to add runs to table

--- scripts/speedrunScraper.js
+++ scripts/speedrunScraper.js
@@ -12,28 +12,31 @@ platforms = {
 
 async function getResults() {
   const response = await fetch(
-    "https://www.speedrun.com/api/v1/runs?max=200&game=om1mx362"
+    "https://www.speedrun.com/api/v1/runs?max=200&offset=0&game=k6qre01g"
   );
   const data = await response.json();
   for (let i = 0; i < 200; i++) {
-    let platform = await fetch(
-      `https://www.speedrun.com/api/v1/platforms/${data.data[i].system.platform}`
-    );
-    platform = await platform.json();
-    if (
-      data.data[i].submitted !== null &&
-      platforms[platform.data.name] !== undefined
-    ) {
-      // console.log(data.data[i].status.status == "verified" ? 1 : 0);
-      console.log(
-        `(${Math.round(Math.random() * 3 + 1)}, 1, 1, ${
-          platforms[platform.data.name]
-        }, 1, ${data.data[i].times.primary_t}, ${
-          data.data[i].times.ingame_t
-        }, 1.64, '${data.data[i].submitted}', '${
-          data.data[i].videos.links[0].uri
-        }', ${data.data[i].status.status == "verified" ? 1 : 0}),`
+    if (data.data[i].system !== undefined) {
+      let platform = await fetch(
+        `https://www.speedrun.com/api/v1/platforms/${data.data[i].system.platform}`
       );
+      platform = await platform.json();
+      if (
+        data.data[i].submitted !== null &&
+        platforms[platform.data.name] !== undefined &&
+        data.data[i].videos.links !== undefined
+      ) {
+        // console.log(data.data[i].status.status == "verified" ? 1 : 0);
+        console.log(
+          `(${Math.round(Math.random() * 3 + 1)}, 2, 1, ${
+            platforms[platform.data.name]
+          }, 1, ${data.data[i].times.primary_t}, ${
+            data.data[i].times.ingame_t
+          }, 4.01, '${data.data[i].submitted}', '${
+            data.data[i].videos.links[0].uri
+          }', ${data.data[i].status.status == "verified" ? 1 : 0}),`
+        );
+      }
     }
   }
 }

9694516d28af61e95c5910c0c04142f44d2b8106
Daniel Haupt <danielh@bbd.co.za>
2023-02-23 15:37:34 +0200

    moved insert to different file

--- scripts/alterTables.sql
+++ scripts/alterTables.sql
@@ -3,10 +3,10 @@ GO
 
 ALTER TABLE dbo.Runs
     ADD deleted BIT NOT NULL 
-    CONSTRAINT default_value DEFAULT 0
+    CONSTRAINT default_value_runs DEFAULT 0
 GO
 
 ALTER TABLE dbo.Runners
     ADD deleted BIT NOT NULL 
-    CONSTRAINT default_value DEFAULT 0
+    CONSTRAINT default_value_runners DEFAULT 0
 GO
